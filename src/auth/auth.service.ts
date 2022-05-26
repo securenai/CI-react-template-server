0c9cc3903bf32fd150ea2f240bd27b9f431749f9
naishiuan <michaelnccu@gmail.com>
2022-05-26 17:41:34 +0800

    JWT Token Auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(name: string, password: string): Promise<any> {
     console.log('222', name + password);
@@ -13,5 +17,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    console.log(user);
+    const payload = { username: user.name, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
